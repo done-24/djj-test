7360548eaaecfc0ed0b1a2308fd43f18c85a912e
summer_fi2021 <summer_fi2021@163.com>
2023-02-05 02:22:47 +0800

    feat: commander test

--- bin/index.js
+++ bin/index.js
@@ -1,55 +1,55 @@
 #!/usr/bin/env node
 
-const yargs = require('yargs/yargs')
-const { hideBin } = require('yargs/helpers');
-const dedent = require('dedent');
-const { recommendCommands } = require('yargs');
+const commander = require('commander')
+const pkg = require('../package.json')
+// const { program } = commander
 
-const cli = yargs(hideBin(process.argv))
+const program = new commander.Command()
 
-cli
-.usage('Usage: dyj [command] <options>')
-.demandCommand(1, 'A commond is required. Pass --help to see all available commands and options')
-.strict()
-.recommendCommands()
-.fail((err, msg) => {
-    console.log('err', err)
-    console.log('msg', msg)
+program
+    .usage('[command] <options>')
+    .version(pkg.version)
+    .option('-d, --debug', '是否开启调试模式', false)
+    .option('-e, --env <env>', '获取环境变量')
+    
+
+const clone = program.command('clone <source> [destination]')
+clone
+    .description('clone a repository')
+    .option('-f, --force', '是否强制克隆')
+    .action((source, destination, cmdObj) => {
+    console.log('do clone', source,  destination, cmdObj)
 })
-.alias('h', 'help')
-.alias('v', 'version')
-.wrap(cli.terminalWidth())
-.options({
-    debug: {
-        type: 'boolean',
-        describe: 'Bootstrap debug mode',
-        alias: 'd'
-    }
-})
-.option('registry', {
-    type: 'string',
-    describe: 'Define global registry',
-    alias: 'r'
-})
-.group(['debug'], 'Dev Options: ')
-.group(['registry'], 'Extra Options: ')
-.command('init [name]', 'Do init a project', (yargs) => {
-    yargs.option('name', {
-        type: 'string',
-        alias: 'n',
-        describe: 'Name of a project'
+
+// addCommand 注册子命令
+const service = new commander.Command('services')
+service
+    .command('start [port]')
+    .description('start service by port')
+    .action(port => {
+        console.log(port)
     })
-}, (argv) => {
-    console.log(argv)
-})
-.command({
-    command: 'configure <key> [value]',
-    aliases: ['config', 'cfg'],
-    desc: 'Set a config variable',
-    builder: (yargs) => yargs.default('value', 'true'),
-    handler: (argv) => {
-      console.log(`setting ${argv.key} to ${argv.value}`)
-    }
-})
-.epilogue(dedent`when a commnd fails, all logs will print`)
-.argv;
+
+const opts = program.opts()
+console.log(opts)
+
+program.addCommand(service)
+
+// program
+//     .command('install [name]', 'install package', {
+//         executableFile: 'dyj-cli',
+//         isDefault: true,
+//         // hidden: true
+//     })
+
+program
+    .arguments('<cmd> [options]')
+    .description('test command', {
+        cmd: 'command to run',
+        options: 'options for command'
+    })
+    .action((cmd, options) => {
+        console.log(cmd,options )
+    })
+
+program.parse(process.args)
